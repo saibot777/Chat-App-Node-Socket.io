ebd8f09117ff94701c4c191c9fbcf5cb949ac0a9
saibot777 <stefantrajkovic777@gmail.com>
2017-01-20 17:25:42 +0100

    timestamps with moment.js, some UI-fixes...

--- public/js/index.js
+++ public/js/index.js
@@ -9,18 +9,19 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-  console.log('newMessage', message);
+  var formattedTime = moment(message.createdAt).format('h:mm a');
   var li = jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
   jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
   var li = jQuery('<li></li>');
   var a = jQuery('<a target="_blank">My current location</a>');
 
-  li.text(`${message.from}: `);
+  li.text(`${message.from} ${formattedTime}: `);
   a.attr('href', message.url);
   li.append(a);
   jQuery('#messages').append(li);
@@ -48,13 +49,13 @@ locationButton.on('click', function () {
   locationButton.attr('disabled', 'disabled').text('Sending Location...');
 
   navigator.geolocation.getCurrentPosition(function (position) {
-    locationButton.removeAttr('disabled').text('Send Location...');
+    locationButton.removeAttr('disabled').text('Send Location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
-    locationButton.removeAttr('disabled').text('Send Location...');
+    locationButton.removeAttr('disabled').text('Send Location');
     alert('Unable to fetch location.');
   });
 });
