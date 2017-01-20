1ba42cf433d0490aa0a7390feb34addf991d25f5
saibot777 <stefantrajkovic777@gmail.com>
2017-01-20 14:50:52 +0100

    code refactored, UI created...

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,28 @@
 var socket = io();
 
-socket.on('connect', function() {
+socket.on('connect', function () {
   console.log('Connected to server');
 });
 
-socket.on('disconnect', function() {
+socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function(message) {
+socket.on('newMessage', function (message) {
   console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
 });
 
-socket.emit('createMessage', {
-  from: 'Frank',
-  text: 'Hi'
-}, function (data) {
-  console.log('Got it', data);
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+
+  });
 });
